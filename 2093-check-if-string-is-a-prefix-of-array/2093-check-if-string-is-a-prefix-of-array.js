640b769f4ebc4daaedd8923a284a4547c4a60c74
Aman salman <82624209+AmanSalman@users.noreply.github.com>
2024-10-08 14:18:46 +0300

    Time: 46 ms (95.63%), Space: 49.2 MB (71.04%) - LeetHub

--- 2093-check-if-string-is-a-prefix-of-array/2093-check-if-string-is-a-prefix-of-array.js
+++ 2093-check-if-string-is-a-prefix-of-array/2093-check-if-string-is-a-prefix-of-array.js
@@ -3,14 +3,24 @@
  * @param {string[]} words
  * @return {boolean}
  */
+// var isPrefixString = function(s, words) {
+//     for(let i= 0; i < words.length; i++) {
+//         if(s.startsWith(words[i])) {
+//             return true
+//         } else {
+//             return false;
+//         }
+//     }
+// };
+
+
 var isPrefixString = function(s, words) {
+    let str =''
     for(let i= 0; i < words.length; i++) {
-        console.log(s)
-        if(s.startsWith(words[i])) {
-            s = s.replace(words[i], '');
-       } else {
-        break
-       }
+        str = str + words[i]
+        if(str === s) {
+            return true
+        }
     }
-    return s.length ==0 ? true : false
+    return false
 };
